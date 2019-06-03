ea09ec0f2ff6944df1c4d656ded70f962e2c1bd1
SeanShi <1126519721@qq.com>
2019-06-03 23:15:17 +0800

    add

--- CppProTest/Test.cpp
+++ CppProTest/Test.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+void TestUserInput() {
+	cout << "please enter your first name:";
+	string user_first_name;
+	cin >> user_first_name;
+
+	cout << "please enter your last name:";
+	string user_last_name;
+	cin >> user_last_name;
+
+	cout << "Hello, " << user_first_name << " " << user_last_name << " ... and goodbye!\n";
+}
+void TestConst() {
+	const int a = 100;
+	//a = 10;
+}
+
+void TestIncre() {
+	int a = 0;
+	cout << a++ <<"\n" << ++a;
+}
+
+void TestSwitch() {
+	switch (1)
+	{
+	case 1:
+			cout << "1\n";
+			break;
+	default:
+		cout << "1";
+		break;
+	}
+}
 int main() {
-	cout << "type: \t\t" << "************size**************" << endl;
+	//TestUserInput();
+	//TestConst();
+	//TestIncre();
+	printf("---------\n");
+	TestSwitch();
+	printf("---------");
 	return 0;
 }
